ae8fd1056475c8c8dfd2537ccb2cc3c684cfae79
Guido Guenther <agx@sigxcpu.org>
2007-05-04 08:27:52 +0200

    Imported upstream version 0.4.8
    Summary: Imported upstream version 0.4.8
    Keywords:
    
    Imported vobject-0.4.8
    into Git repository

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ Parses iCalendar and vCard files into Python data structures, decoding the relev
 
 Requires python 2.4 or later, dateutil (http://labix.org/python-dateutil) 1.1 or later.
 
-Recent changes: Merge in Apple CalendarServer patches, improve recurring VTODO support
+Recent changes:
+- Fixed UNTIL values in RRULEs to have the right value when a dateutil rruleset is created
+- Fixed a problem causing DATE valued RDATEs and EXDATEs to be ignored when interpreting recurrence rules
+- Added an ics_diff module and an ics_diff command line script for comparing the VEVENTs and VTODOs in similar iCalendar files
 
 For older changes, see http://vobject.skyhouseconsulting.com/history.html or http://websvn.osafoundation.org/listing.php?repname=vobject&path=/trunk/
 
 """
 
-# not using setuptools until Chandler's ready for eggs
 from ez_setup import use_setuptools
 use_setuptools()
 
@@ -21,7 +23,7 @@ from setuptools import setup, find_packages
 
 # Metadata
 PACKAGE_NAME = "vobject"
-PACKAGE_VERSION = "0.4.4"
+PACKAGE_VERSION = "0.4.8"
 
 ALL_EXTS = ['*.py', '*.ics', '*.txt']
 
@@ -36,7 +38,7 @@ setup(name = "vobject",
       license = "Apache",
       zip_safe = True,
       url = "http://vobject.skyhouseconsulting.com",
-
+      entry_points = { 'console_scripts': ['ics_diff = vobject.ics_diff:main'] },
       package_dir = {'':'src'},
       package_data = {'': ALL_EXTS},
 
@@ -47,7 +49,7 @@ setup(name = "vobject",
       description = doclines[0],
       long_description = "\n".join(doclines[2:]),
       classifiers =  """
-      Development Status :: 3 - Alpha
+      Development Status :: 4 - Beta
       Environment :: Console
       License :: OSI Approved :: BSD License
       Intended Audience :: Developers
